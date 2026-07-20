c16993f2e3d180510b17757fba109cd67d293b31
Your Name <you@example.com>
2026-07-20 12:56:05 +0530

    app.py 3019 n .env base url

--- app.py
+++ app.py
@@ -619,4 +619,4 @@ if __name__ == '__main__':
     scheduler.add_job(func=check_and_send_scheduled_reports, trigger="interval", minutes=1)
     scheduler.start()
 
-    app.run(host="0.0.0.0", port=5005, debug=True, use_reloader=False)
+    app.run(host="0.0.0.0", port=3019, debug=True, use_reloader=False)
